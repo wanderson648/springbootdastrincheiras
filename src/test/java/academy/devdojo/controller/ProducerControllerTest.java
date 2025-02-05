e34c2e764b806f1ce07bcb0de04ca3dd40d972c7
wanderson648 <testepedroca@gmail.com>
2025-02-05 15:48:31 -0300

    implementa arquivo de resourceLoader

--- src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -1,29 +1,29 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
-import academy.devdojo.mapper.ProducerMapper;
-import academy.devdojo.mapper.ProducerMapperImpl;
 import academy.devdojo.repository.ProducerData;
-import academy.devdojo.repository.ProducerHardCodeRepository;
-import academy.devdojo.service.ProducerService;
 import org.junit.jupiter.api.*;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebMvcTest(controllers = ProducerController.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Import({ProducerMapperImpl.class, ProducerService.class, ProducerHardCodeRepository.class, ProducerData.class})
+@ComponentScan(basePackages = "academy.devdojo")
 class ProducerControllerTest {
 
     @Autowired
@@ -32,13 +32,20 @@ class ProducerControllerTest {
     @MockBean
     private ProducerData producerData;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     private List<Producer> producerList = new ArrayList<>();
 
     @BeforeEach
     void init() {
-        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
-        var witSudio = Producer.builder().id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
-        var studioGibli = Producer.builder().id(3L).name("Studio Gibli").createdAt(LocalDateTime.now()).build();
+        var dateTime = "2025-02-05T15:05:30.9363996";
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+        var localDateTime = LocalDateTime.parse(dateTime, formatter);
+
+        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(localDateTime).build();
+        var witSudio = Producer.builder().id(2L).name("Wit Studio").createdAt(localDateTime).build();
+        var studioGibli = Producer.builder().id(3L).name("Studio Gibli").createdAt(localDateTime).build();
         producerList.addAll(List.of(ufotable, witSudio, studioGibli));
     }
 
@@ -47,10 +54,17 @@ class ProducerControllerTest {
     @Order(1)
     void findAll_ReturnsAllProducers_WhenArgumentIsNull() throws Exception {
         BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+        var response = readResourceFile("producer/get-producer-null-name-200.json");
+
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws IOException {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }
